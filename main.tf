062f3a5701608643428336486ba2ca53c5e60e5e
franklin-instantteams <frank.lin@instantteams.com>
2022-11-16 23:01:48 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,14 @@
 resource "google_storage_bucket" "gcs_bucket" {
   name = "test-bucket-random-001123"
+<<<<<<< Updated upstream
 }
 
 resource "google_storage_bucket" "gcs_bucket" {
   name = "test-bucket-random-001123"
+=======
+  location      = "US"
+  force_destroy = true
+
+  public_access_prevention = "enforced"
+>>>>>>> Stashed changes
 }
