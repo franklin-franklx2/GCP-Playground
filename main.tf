a5997824a704dd42f188336197e8e5b98a9ddda7
franklin-instantteams <frank.lin@instantteams.com>
2022-11-16 23:15:08 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "google_storage_bucket" "gcs_bucket_0001" {
+resource "google_storage_bucket" "franklx2_gcs_bucket_0001" {
   name = "test-bucket-random-001123"
   location      = "US"
   force_destroy = true
